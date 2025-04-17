0ac86705432faea2e4b89005d1173616f4533020
子真 <zizhen0528@163.com>
2025-04-17 20:27:42 +0800

    issue:fxi bugs

---
+++ entry.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+chpasswd < /root/passwd
+
+if [ -f /meex/.init.sh ];then
+    /meex/.init.sh
+    rm -f /meex/.init.sh
+fi
+if [ -f /meex/.startup.sh ];then
+    /meex/.startup.sh
+fi
+/usr/sbin/sshd -D
